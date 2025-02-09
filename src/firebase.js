e597b64402f018bb8406d51f52b55c1d27d57687
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2025-02-09 18:15:09 +0700

    update config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDURDov7yNWbfEw5tK-omQjYOI-ne0wq0s',
-  authDomain: 'cloud-storage-fed0f.firebaseapp.com',
-  projectId: 'cloud-storage-fed0f',
-  storageBucket: 'cloud-storage-fed0f.firebasestorage.app',
-  messagingSenderId: '696679707723',
-  appId: '1:696679707723:web:1e05d61a167cbcca8f49ce',
-  measurementId: 'G-MWHVRQL66N',
-};
+import firebaseConfig from './firebase-config.json';
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
